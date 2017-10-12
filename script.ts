e070ce2ef327564ac83039081860d140f874b869
DWW1993 <daniel.w.walker1993@gmail.com>
2017-10-12 16:56:01 -0500

    scaffolding

---
+++ script.ts
@@ -0,0 +1,21 @@
+
+
+class Shape {
+
+}
+
+class Square extends Shape {
+
+}
+
+class Rectangle extends Shape {
+    
+}
+
+class Triangle extends Shape {
+    
+}
+
+class Circle extends Shape {
+    
+}
